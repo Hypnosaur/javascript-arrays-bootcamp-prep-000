180310decc14d39111def05b862b1e8d71959758
Laura Fretter <fretterlaura@gmail.com>
2019-02-28 12:30:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,8 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray(array, element) {
   
-   return array.unshift(element)
+    array.unshift(element)
+    return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
